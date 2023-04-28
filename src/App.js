ca168f71b9b97670a88bb724064e48c027a62c33
bob <robert@mail.ru>
2023-04-28 21:06:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+import './App.css';
+import black_pawn from "./black_pawn.png";
+import white_pawn from "./white_pawn.png";
+
+
+let matrix = [
+  [null, null, null, null, null, null, null, null],
+  ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null]
+];
+
+function Board() {
+  const [secondClick, setSecondClick] = useState(false);
+  const [squareClicked, setSquareClicked] = useState();
+  const [squares, setSquares] = useState(matrix);
+
+  let b = [];
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      b.push(<Square figure={squares[row][col]} onSquareClick={() => handleClick(row, col)} />);
+    }
+  }
+
+  function handleClick(row, col) {
+    if (secondClick === false) {
+      if (squares[row][col] === "P") {
+        setSecondClick(true);
+        setSquareClicked([row, col]);
+      }
+    } else {
+      if (row !== squareClicked[0] || col !== squareClicked[1]) {
+        const nextSquares = squares.slice();
+        setSecondClick(false);
+        nextSquares[row][col] = "P";
+        nextSquares[squareClicked[0]][squareClicked[1]] = null;
+        setSquares(nextSquares);
+      }
+
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='board'>
+      {b}
     </div>
   );
 }
 
-export default App;
+function Square({ figure, onSquareClick }) {
+  let f;
+
+  if (figure === "P") {
+    f = <Pawn />;
+  } else {
+    f = "";
+  }
+
+  return (
+    <div className="square" onClick={onSquareClick}>
+      {f}
+    </div>
+  );
+}
+
+function Pawn() {
+  return (
+    <img className="pawn" src={black_pawn} alt="pawn" />
+  );
+}
+
+export default Board;
